c985587212e67a27de8b802729b4801ce4ad4a88
Your Name <you@example.com>
2021-06-07 13:18:54 -0400

    basic auth

--- api/app/app.js
+++ api/app/app.js
@@ -21,11 +21,12 @@ connect.then((db) => {
 }, (err) => { console.log(err)});
 
 
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/users');
 
 const dishRouter = require('./routes/dishRouter');
 const promoRouter = require('./routes/promoRouter');
 const leaderRouter = require('./routes/leaderRouter');
-
 
 var app = express();
 
@@ -37,12 +38,48 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+
+function auth(req, res ,next){
+  console.log(req.headers);
+
+  var authHeader = req.headers.authorization;
+
+  if (!authHeader){
+    let err = new Error('You are not authenticated');
+
+    res.setHeader('WWW-Authenticate','Basic');
+    err.status = 401;
+    return next(err);
+  }
+
+  let auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+
+  let username = auth[0];
+  let password = auth[1];
+
+  if (username === 'admin' && password === "password"){
+    next();
+  }
+  else{
+    let err = new Error('You are not authenticated');
+    res.setHeader('WWW-Authenticate','Basic');
+    err.status = 401;
+    return next(err);
+  }
+}
+
+app.use(auth);
+
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/',indexRouter);
+app.use('/user',userRouter);
 
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
